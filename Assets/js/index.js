f2a9eb5f8ef9c1d22be72a2ed3759273cd5f72e9
Maiyoj <valentinemaiyo1995@gmail.com>
2022-05-13 23:57:01 +0300

    add dipslay all div class

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -3,10 +3,19 @@
 let start = document.querySelector('.start');
 let quiz = document.querySelector('.questions');
 let welcome = document.querySelector('.head');
+let quiz1 = document.querySelector('#qn1');
+let quiz2 = document.querySelector('#qn2');
+let all = document.querySelector('.all');
+//display block
 
 
 //function hide and display elements
 function startQuiz(){
-    quiz.style.display ="block";
+    all.style.display ="block";
     welcome.style.display ="none";
+   
+}
+//function onclicknext
+function next(id){
+    
 }
