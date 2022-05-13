b47781977ade070e19b222b8ff5c0dbadcd124c8
Maiyoj <valentinemaiyo1995@gmail.com>
2022-05-13 20:45:13 +0300

    add display and hide elements function

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,8 +2,13 @@
 //Start Fuction
 let start = document.querySelector('.start');
 let quiz = document.querySelector('.questions');
+let welcome = document.querySelector('.head')
 
 //hide questions
 quiz.style.display = "none";
 
-
+//function hide and display elements
+function startQuiz(){
+    quiz.style.display ="block";
+    welcome.style.display ="none";
+}
