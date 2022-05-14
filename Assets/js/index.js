ff4caab399f647d9b19b1dbbf0cee0c3f28ce8c4
Maiyoj <valentinemaiyo1995@gmail.com>
2022-05-14 10:47:58 +0300

    add display board

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -6,6 +6,7 @@ let welcome = document.querySelector('.head');
 let quiz1 = document.querySelector('#qn1');
 let quiz2 = document.querySelector('#qn2');
 let all = document.querySelector('.all');
+let board = document.querySelector('.board');
 //display block
 
 
@@ -39,6 +40,29 @@ function results(){
       if (document.getElementById('choiceCorrect4').checked){
         score +=25;
     }
-    alert("Your score is" + score);
+    //alert("Your score is" + score);
+    board.innerHTML = "your score is " + score;
 
+}
+
+//Performance output
+var score = 80;
+
+switch (score){
+
+  case 100:
+    alert("You did great!" + "your score is " +score);
+    break;
+  
+  case 90:
+    console.log("You did good!");
+    break;
+
+  
+  case 10:
+    console.log("YOU FAILED!");
+    break;
+
+  default:
+    console.log(score,"is not a letter grade");
 }
